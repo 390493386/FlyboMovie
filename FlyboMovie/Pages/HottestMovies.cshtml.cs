3211cd13daa2b75aed11853d3a725b68c3d960c4
Skyder Zhan <professor_zh@msn.cn>
2018-10-18 18:47:31 +0800

    改版

---
+++ FlyboMovie/Pages/HottestMovies.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using FlyboMovie.Dtos;
+using FlyboMovie.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace FlyboMovie.Pages
+{
+    public class HottestMoviesModel : PageModel
+    {
+        private IMovieService _movieService;
+
+        public readonly string TabName = "HottestMovies";
+        public IList<MovieLiteDto> HottestMovies { get; set; }
+
+        public HottestMoviesModel(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        public void OnGet(string keywords)
+        {
+            HottestMovies = _movieService.GetHotestMovies();
+        }
+    }
+}
---
+++ FlyboMovie/Pages/LatestMovies.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlyboMovie.Dtos;
+using FlyboMovie.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlyboMovie.Pages
+{
+    public class LatestMoviesModel : PageModel
+    {
+        private IMovieService _movieService;
+        public readonly string TabName = "LatestMovies";
+
+        public IList<MovieLiteDto> LatestMovies { get; set; }
+
+        public LatestMoviesModel(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        public void OnGet(string keywords)
+        {
+            LatestMovies = _movieService.GetLatestMovies();
+        }
+    }
+}
